75dae435c028c773bc453aeea466a5527b261b46
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-04-06 14:33:28 +0000

    task100

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-""" a module to pack and deploy a package to servers """
-from fabric.operations import put, run
-from datetime import datetime
-from os.path import isfile
+""" a module to packag web_static files and deploy """
+import datetime
+import os
 from fabric.api import put, env, run, local
 
 
@@ -11,77 +10,51 @@ env.hosts = ["35.231.213.145", "34.234.65.186"]
 env.user = "ubuntu"
 
 
-def do_pack():
-    """ Generates a .tgz archive from the contents of the web_static folder """
-    try:
-        current_time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-        local("mkdir -p versions")
-        filename = "web_static_{}.tgz".format(current_time)
-        local("tar -czvf versions/{} web_static".format(filename))
-        return "versions/{}".format(filename)
-    except:
-        return None
-
 def do_deploy(archive_path):
-    """ deploys archive to web servers """
-    if not isfile(archive_path):
+    """ deploy package """
+    if archive_path is None or not os.path.isfile(archive_path):
+        print("NOT PATH")
         return False
 
-    try:
-        # Upload archive to the server
-        put(archive_path, "/tmp/")
+    aname = os.path.basename(archive_path)
+    rname = aname.split(".")[0]
 
-        # Create folder to extract files to
-        file_name = archive_path.split("/")[-1]
-        folder_name = file_name.split(".")[0]
-        run("sudo mkdir -p /data/web_static/releases/{}".format(folder_name))
+    put(local_path=archive_path, remote_path="/tmp/")
+    run("mkdir -p /data/web_static/releases/{}".format(rname))
+    run("tar -xzf /tmp/{} \
+        -C /data/web_static/releases/{}".format(aname, rname))
+    run("rm /tmp/{}".format(aname))
+    run("rm -rf /data/web_static/current")
+    run("ln -fs /data/web_static/releases/{}/ \
+        /data/web_static/current".format(rname))
+    run("mv /data/web_static/current/web_static/* /data/web_static/current/")
+    run("rm -rf /data/web_static/curren/web_static")
 
-        # Extract archive contents into folder
-        run("sudo tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(file_name, folder_name))
+    return True
 
-        # Remove archive
-        run("sudo rm /tmp/{}".format(file_name))
 
-        # Move files into correct folder
-        run("sudo mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}/"
-            .format(folder_name, folder_name))
+def do_pack():
+    """ package function """
+    if not os.path.isdir("./versions"):
+        os.makedirs("./versions")
+    ntime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    local("tar -czzf versions/web_static_{}.tgz web_static/*".format(ntime))
+    return ("{}/versions/web_static_{}.tgz".format(os.path.dirname(
+        os.path.abspath(__file__)), ntime))
 
-        # Remove unneeded folder
-        run("sudo rm -rf /data/web_static/releases/{}/web_static"
-            .format(folder_name))
-
-        # Remove old symbolic link
-        run("sudo rm -rf /data/web_static/current")
-
-        # Create new symbolic link
-        run("sudo ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(folder_name))
-
-        return True
-    except Exception:
-        return False
 
 def deploy():
-    """ packs and deploys an archive to the web servers """
-    archive_path = do_pack()
-    if archive_path is None:
+    """ package && deploy to servers """
+    path = do_pack()
+    if path is None:
         return False
-    return do_deploy(archive_path)
+    return(do_deploy(path))
+
 
 def do_clean(number=0):
-    """ deletes out-of-date archives """
+    """ clean up old stuff every time """
     number = int(number)
-
-    if number < 0:
-        return
-
-    number = 1 if number == 0 else number
-
-    with cd('/data/web_static/releases'):
-        run('ls -1tr | tail -n +{} | xargs -I {} rm -rf {{}}'.format(number + 1))
-
-    with lcd('versions'):
-        local('ls -1t | tail -n +{} | xargs -I {} rm -rf {{}}'.format(number + 1))
-
+    local("ls -d -1tr versions/* | tail -n +{} | \
+          xargs -d '\n' rm -f --".format(2 if number < 1 else number + 1))
+    run("ls -d -1tr /data/web_static/releases/* | tail -n +{} | \
+          xargs -d '\n' rm -rf --".format(2 if number < 1 else number + 1))
