41087e7fbbff840a3b83339fe910977a7ca99c54
Mathapelo Mphahlele <modiegi.mphahlele93@gmail.com>
2023-04-24 05:09:33 -0700

    fix errors

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """ Starts a Flask web application """
 
+from models.city import City
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from models.city import City
 
 app = Flask(__name__)
 
 
+@app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
     """Displays a HTML page with a list of States and Cities"""
     states = storage.all("State")
@@ -22,4 +23,4 @@ def teardown(exc):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host="0.0.0.0", port='5000')
